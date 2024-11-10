e1f17ba6d0716092e617d5c92b96c8c84262fb41
Matt Gleich <git@mattglei.ch>
2024-11-09 19:05:53 -0500

    feat: refresh a access token
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- backend/internal/spotify/tokens.go
+++ backend/internal/spotify/tokens.go
@@ -1,18 +1,25 @@
 package spotify
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"time"
 
+	"github.com/gleich/lumber/v3"
 	"github.com/gleich/squeue/internal/secrets"
 )
 
 type tokens struct {
 	Access    string `json:"access_token"`
 	Refresh   string `json:"refresh_token"`
-	ExpiresAt int64  `json:"expires_at"` // expected to not be in the refresh data (aka nil)
+	ExpiresAt int64  `json:"expires_at"`
 }
 
-func loadTokens() tokens {
+func LoadTokens() tokens {
 	return tokens{
 		Access:    secrets.SECRETS.AccessToken,
 		Refresh:   secrets.SECRETS.RefreshToken,
@@ -20,9 +27,57 @@ func loadTokens() tokens {
 	}
 }
 
-func (t *tokens) refreshIfNeeded() {
-	// subtract 30 to ensure that token gets refreshed a few seconds before expiring
-	if t.ExpiresAt-5 >= time.Now().Unix() {
+func (t *tokens) RefreshIfNeeded() {
+	// add 60 to ensure that the token doesn't expire in the next 60 seconds
+	if t.ExpiresAt-60 >= time.Now().Unix() {
 		return
 	}
+
+	params := url.Values{
+		"refresh_token": {t.Refresh},
+		"grant_type":    {"refresh_token"},
+		"client_id":     {secrets.SECRETS.ClientID},
+	}
+	req, err := http.NewRequest(
+		"POST",
+		"https://accounts.spotify.com/api/token?"+params.Encode(),
+		nil,
+	)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set(
+		"Authorization",
+		"Basic "+base64.StdEncoding.EncodeToString(
+			[]byte(fmt.Sprintf("%s:%s", secrets.SECRETS.ClientID, secrets.SECRETS.ClientSecret)),
+		),
+	)
+	if err != nil {
+		lumber.Error(err, "creating request for new token failed")
+		return
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		lumber.Error(err, "sending request for new token data failed")
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		lumber.Error(err, "reading response body failed")
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		lumber.ErrorMsg(resp.StatusCode, "when trying to get new token data:", string(body))
+	}
+
+	var tokens tokens
+	err = json.Unmarshal(body, &tokens)
+	if err != nil {
+		lumber.Error(err, "failed to parse json")
+		lumber.Debug("body:", string(body))
+		return
+	}
+
+	*t = tokens
+	lumber.Done(fmt.Sprintf("loaded new tokens [access_token = %s]", t.Access))
 }
--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/gleich/squeue/internal/secrets"
+	"github.com/gleich/squeue/internal/spotify"
 )
 
 func main() {
 	secrets.Load()
+	tokens := spotify.LoadTokens()
+	tokens.RefreshIfNeeded()
 }
